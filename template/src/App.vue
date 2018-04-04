4591f68a6cafce4c33cb3fbb83a442e785bb754c
leechikit <leechikit0823@163.com>
2018-04-04 16:56:14 +0800

    update

--- template/src/App.vue
+++ template/src/App.vue
@@ -30,7 +30,7 @@ export default {
 }
 </script>
 
-<style{{#sass}} lang="scss"{{/sass}}>
+<style lang="scss">
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
